a69a251a160c9872934121afb1cb2094c4eb3a22
coder1 <c.o.d.e.r.1@mail.ru>
2022-05-29 14:01:14 +0500

    Управление с клавиатуры для компонента 'archive' (правки)

--- src/components/archive/Archive.ts
+++ src/components/archive/Archive.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { boundMethod } from 'autobind-decorator';
 
 class Archive {
@@ -7,6 +8,8 @@ class Archive {
 
   months: HTMLElement | null = null;
 
+  links: NodeListOf<Element> | null = null;
+
   constructor(className: string, elem: Element) {
     this.year = elem as HTMLElement;
     this.init();
@@ -15,6 +18,7 @@ class Archive {
   init() {
     if (!this.year) return false;
     this.months = this.year.querySelector('.archive__months');
+    this.links = this.year.querySelectorAll('.archive__articles-link');
     this.bindEvent();
 
     return true;
@@ -31,17 +35,25 @@ class Archive {
 
   @boundMethod
   handleMonthsKeyDown(e: KeyboardEvent) {
+    const currentTarget = e.currentTarget as HTMLElement;
     if (e.code === 'Escape') {
       this.closeList();
+      this.toggleLinksAvailability();
+      currentTarget.focus();
     }
 
     const target = e.target as HTMLElement;
-    const condition = !target.classList.contains('archive__articles-link')
-      && (e.code === 'Enter' || e.code === 'Space');
+    const isLink = target.classList.contains('archive__articles-link');
+    const condition = !isLink && (e.code === 'Enter' || e.code === 'Space');
 
     if (condition) {
       e.preventDefault();
       this.toggleList();
+      this.toggleLinksAvailability();
+    }
+
+    if (isLink && e.code === 'Space') {
+      e.preventDefault();
     }
   }
 
@@ -55,6 +67,16 @@ class Archive {
     if (!this.months) return false;
     this.months.classList.add('archive__months_visually-hidden');
     return true;
+  }
+
+  toggleLinksAvailability() {
+    if (this.months && this.links) {
+      if (this.months.classList.contains('archive__months_visually-hidden')) {
+        this.links.forEach((link) => link.setAttribute('tabindex', '-1'));
+      } else {
+        this.links.forEach((link) => link.removeAttribute('tabindex'));
+      }
+    }
   }
 
   bindEvent() {
