786f0a727c271c6fd9ea45232ff6e863f3fc387c
coder1 <c.o.d.e.r.1@mail.ru>
2022-06-13 15:45:03 +0500

    Добавлен рассчет высоты ползунка

--- src/components/tags/Tags.ts
+++ src/components/tags/Tags.ts
@@ -12,18 +12,38 @@ class Tags {
 
   tags: HTMLElement | null = null;
 
+  tagsList: HTMLElement | null = null;
+
+  frame: HTMLElement | null = null;
+
+  thumb: HTMLElement | null = null;
+
   constructor(className: string, elem: Element) {
     this.wrapper = elem as HTMLElement;
     this.className = className.replace('.tags-wrapper', 'tags');
     this.init();
+    this.renderThumb();
   }
 
   init() {
     if (!this.wrapper) return false;
     this.track = this.wrapper.querySelector(`.${this.className}__scrollbar-track`);
     this.tags = this.wrapper.querySelector(`.${this.className}`);
+    this.frame = this.wrapper.querySelector(`.${this.className}-frame`);
+    this.thumb = this.wrapper.querySelector(`.${this.className}__scrollbar-thumb`);
     this.bindEvent();
 
+    return true;
+  }
+
+  renderThumb() {
+    if (!this.track || !this.tags || !this.frame || !this.thumb) return false;
+    const trackHeight = this.track.offsetHeight;
+    const tagsHeight = this.tags.offsetHeight;
+    const frameHeight = this.frame.offsetHeight;
+    /* высота элемента tags относится к высоте frame так же, как track к thumb => вычислим высоту thumb (ползунка) */
+    const thumbHeight = (frameHeight * trackHeight) / tagsHeight;
+    this.thumb.style.height = thumbHeight > 20 ? `${thumbHeight}px` : '20px';
     return true;
   }
 
