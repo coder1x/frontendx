ba4dcc36e618a77775e257ddf2be6a929ed07fac
coder1 <c.o.d.e.r.1@mail.ru>
2022-08-13 21:19:22 +0300

    Рефакторинг компонента BreabCrumbs

--- src/components/bread-crumbs/BreadCrumbs.ts
+++ src/components/bread-crumbs/BreadCrumbs.ts
@@ -9,7 +9,7 @@ class BreadCrumbs {
 
   private markers: HTMLElement | null = null;
 
-  private maxHeight: number = 0;
+  private lineHeight: number = 0;
 
   constructor(className: string, elem: Element) {
     this.breadCrumbs = elem as HTMLElement;
@@ -20,6 +20,11 @@ class BreadCrumbs {
   private init() {
     this.items = this.breadCrumbs.querySelector(`.${this.className}__items`);
     this.markers = this.breadCrumbs.querySelector(`.${this.className}__markers`);
+
+    if (!this.items || !this.markers) return;
+
+    this.lineHeight = parseFloat(window.getComputedStyle(this.items).lineHeight);
+
     this.makeMarkers();
     this.bindEvent();
   }
@@ -31,23 +36,24 @@ class BreadCrumbs {
 
   @boundMethod
   private makeMarkers() {
-    if (!this.items || !this.markers) return;
-    this.maxHeight = this.items.offsetHeight;
-    console.log('this.maxHeight>>>', this.maxHeight);
+    if (!this.markers || !this.items) return;
 
+    // удаляем существующие галочки
     const marks = this.markers.querySelectorAll(`.${this.className}__marker`);
     marks.forEach((mark) => mark.remove());
 
-    const addMarkers = (wrapper: HTMLElement, maxHeight: number) => {
+    const maxHeight = this.items.offsetHeight;
+    const count = Math.round(maxHeight / this.lineHeight);
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < count; i += 1) {
       const element = document.createElement('li');
       element.innerText = '>';
       element.className = `${this.className}__marker`;
-      wrapper.append(element);
-      if (wrapper.offsetHeight < maxHeight) {
-        addMarkers(wrapper, maxHeight);
-      }
-    };
-    addMarkers(this.markers, this.maxHeight);
+      fragment.append(element);
+    }
+
+    this.markers.append(fragment);
   }
 }
 
