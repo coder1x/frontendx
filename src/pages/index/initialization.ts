3e43329876b37db0a293d76f0562202580b7ba2b
Kliushin Anton <c.o.d.e.r.1@mail.ru>
2022-11-25 08:57:15 +0500

    подправил стили тегов и доработал пагинацию

--- src/pages/index/initialization.ts
+++ src/pages/index/initialization.ts
@@ -4,7 +4,7 @@ function initialization(className: string) {
   const layout = document.querySelectorAll(className);
 
   if (layout.length === 1) {
-    new Header();
+    new Header().removeSidePanel();
   }
 }
 
